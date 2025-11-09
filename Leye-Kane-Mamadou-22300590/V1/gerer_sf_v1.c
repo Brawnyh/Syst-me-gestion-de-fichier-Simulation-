563ee7f933c629d0b15bdce8560927726d0dadd9
Brawnyh <amadou@LORDI-EI2BGPO41.localdomain>
2025-11-09 21:19:02 +0100

    Setup et test a faire

--- Leye-Kane-Mamadou-22300590/V1/gerer_sf_v1.c
+++ Leye-Kane-Mamadou-22300590/V1/gerer_sf_v1.c
@@ -3,7 +3,18 @@
 #include "inode.h"
 int main(void){
     tInode inode;
-    //AfficherInode(inode);
-    printf("H");
+    printf("---Test bloc---");
+
+
+    printf("Fin Test");
+    printf("---Test Inode---");
+    printf("=========Inode=========[%d]\n",Numero(inode));
+    printf("type: %d \n",Type(inode));
+    printf("taille: %d Octets\n",Taille(inode));
+    printf("date dernier accès : %d\n",DateDerAcces(inode));
+    printf("date dernière modification : %d\n", DateDerModif(inode));
+    printf("date dernière modification inode : %d\n",DateDerModifFichier(inode));
+    //printf("Données:\n %d \n",AfficherInode(inode)); à finir 
+    
     return 0;
 }
